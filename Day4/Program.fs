9747612886c4e8905f9fd598fc92a338cc828d40
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2022-12-05 14:47:02 -0500

    refactor, add second objective

--- Day4/Program.fs
+++ Day4/Program.fs
@@ -1,33 +1,44 @@
 module Program = 
     let [<EntryPoint>] main args = 
+
+        let getFullyContainsCount (elves: string[][]) = 
+            let mutable count = 0;
+            // loop each inner item and print
+            for pair in elves do
+                // split each tuple by dash
+                let left = pair.[0].Split('-')
+                let right = pair.[1].Split('-')
+                //printfn "%s %s %s %s" left.[0] left.[1] right.[0] right.[1]
+
+                if ((int left.[0] <= int right.[0] && int left.[1] >= int right.[1]) 
+                    || (int right.[0] <= int left.[0]) && int right.[1] >= int left.[1]) then
+                    count <- count + 1
+            count
+
+        let getPartiallyContainsCount (elves: string[][]) = 
+            let mutable count = 0;
+
+            for pair in elves do
+                // split each tuple by dash
+                let left = pair.[0].Split('-')
+                let right = pair.[1].Split('-')
+                
+                if ((int left.[0] <= int right.[0] && int right.[0] <= int left.[1])
+                || (int right.[0] <= int left.[0] && int left.[0] <= int right.[1])) then
+                    count <- count + 1
+
+            count
         
         // read file and print all lines
         let lines = System.IO.File.ReadAllLines("input.txt")
-
-        // // print total pairs
-        printfn "Total lines: %d" lines.Length
         
         // split each line by comma and store as tuple of strings
-        let pairs = lines |> Array.map (fun line -> line.Split(','))
+        let elves = lines |> Array.map (fun line -> line.Split(','))
+        
+        let fullyContainsCount = elves |> getFullyContainsCount
+        let partiallyContainsCount = elves |> getPartiallyContainsCount
 
-        printfn "Total pairs: %d" pairs.Length
-
-        let mutable count = 0;
-        // loop each inner item and print
-        for pair in pairs do
-            // split each tuple by dash
-            let left = pair.[0].Split('-')
-            let right = pair.[1].Split('-')
-            printfn "%s %s %s %s" left.[0] left.[1] right.[0] right.[1]
-
-            // if left 0 greater than right 0 
-            // or left 0 equals right 0 and left 1 greater than right 1
-            // then increment count
-            if ((int left.[0] <= int right.[0] && int left.[1] >= int right.[1]) 
-                || (int right.[0] <= int left.[0]) && int right.[1] >= int left.[1]) then
-                count <- count + 1
-                printfn "fully contains!"
-
-        printfn "count: %d" count
+        printfn "fully contains count: %d" fullyContainsCount
+        printfn "partially contains count: %d" partiallyContainsCount
 
         0
