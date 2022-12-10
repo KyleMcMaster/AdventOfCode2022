ac3c310e4262d75c6e6a58b0c1db5b93d001737d
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2022-12-10 16:54:39 -0500

    Init commit Day 11

---
+++ Day11/Program.fs
@@ -0,0 +1,16 @@
+module Part1 =
+
+    let solve lines = 0
+
+module Part2 =
+
+    let solve lines = 0
+
+
+module Program =
+    [<EntryPoint>]
+    let main args =
+
+        let lines = System.IO.File.ReadAllLines("input.txt")
+
+        0
